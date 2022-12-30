c62d35cb6f8ee0f63df82b5155de40d94283b572
Dean Larbi <deanlarbi@Deans-MBP.hsd1.co.comcast.net>
2022-12-30 11:25:16 -0700

    allow options

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function(req: express.Request, res: express.Response, next: any) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization");
 	next();
 });
