f6c9a759d96335f2130294a52d14f52996761239
Dean Larbi <deanlarbi@Deans-MBP.hsd1.co.comcast.net>
2022-12-30 21:13:53 -0700

    email test

--- modules/gmail-send/GmailSend.ts
+++ modules/gmail-send/GmailSend.ts
@@ -9,8 +9,8 @@ export class GmailSend {
     this.transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD
+        user: 'dean@willieai.com',
+        pass: 'Postal9curse$swaN'
       }
     });
   }
