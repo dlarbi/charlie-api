bf9a59bf40bb41bfcc02f1e026f65f076ab67baa
Dean Larbi <deanlarbi@Deans-MacBook-Pro.local>
2023-01-12 14:17:26 -0600

    ignore docs and pdfs

--- services/text-scraping-service.ts
+++ services/text-scraping-service.ts
@@ -41,7 +41,7 @@ export class TextScrapingService {
         for (let i = 0; i < urls.length; i++) {
             const url = urls[i];
             const { text, title } = await this.getTextByUrl(url);
-            if (url.indexOf('.xml') === -1) {
+            if (url.indexOf('.xml') === -1 && url.indexOf('.pdf') === -1 && url.indexOf('.doc') === -1) {
                 await services.textContentService.saveTextContent({ text, title, url, projectUrl });
                 result.push({ text, title, url, projectUrl });
             }
